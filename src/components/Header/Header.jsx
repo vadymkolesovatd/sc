e9ddc78979bd97a528070891e0a03c0ce4988cde
Vadym Kolesov <v.kolesov@autodoc.eu>
2025-07-03 10:42:49 +0200

    upd

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import icon from "../../assets/icons/icon-192.png";
 import css from "./Header.module.css";
 
 export default function Header() {
   return (
     <header className={css.header}>
+      <img src={icon} className={css.icon} />
       <h1 className={css.title}>Salary calculator</h1>
     </header>
   );
